e4d45593e076c0b6abdbdbdfc1e99f41bea2a252
Phodal Huang <h@phodal.com>
2017-12-17 10:30:26 +0800

    Update Toast.m

--- ios/RCTToast/Toast.m
+++ ios/RCTToast/Toast.m
@@ -1,8 +1,13 @@
 #import <UIKit/UIKit.h>
-#import <React/RCTLog.h>
-#import <React/RCTBridgeModule.h>
 #import "Toast+UIView.h"
 
+#if __has_include(<React/RCTAssert.h>)
+#import <React/RCTLog.h>
+#import <React/RCTBridgeModule.h>
+#else // back compatibility for RN version < 0.40
+#import "RCTLog.h"
+#import "RCTBridgeModule.h"
+#endif
 
 @interface Toast : NSObject <RCTBridgeModule>
 @end
